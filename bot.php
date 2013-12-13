484fde4ffe50899194fa0c32a1c5bb93e86774e7
root <markarneman@gmail.com>
2013-12-13 11:08:37 -0500

    Displaying combined output

--- bot.php
+++ bot.php
@@ -19,14 +19,14 @@ Class Bot {
 	}
 
 	public function events() {
-		if (is_null($this->cache->get('events')))  {
+		//if (is_null($this->cache->get('events')))  {
 			$events['jd'] = $this->joinDOTA();
 			$events['gg'] = $this->gosugamers();
 
 			$events = json_encode($events);
 
 			$this->cache->set('events', $events);
-		} else $events = $this->cache->get('events');
+		//} else $events = $this->cache->get('events');
 
 		$this->sort(json_decode($events, true));
 	}
@@ -36,16 +36,15 @@ Class Bot {
 		{
 			foreach ($events['gg'] as $key => $value) $events['gg'][$key]['match_time'] = strtotime($value['datetime']);
 
-			// jD's API often returns NULL, maybe i'm being throttled; or they still have that template FATAL ERROR on their end
-			if (!is_null($events['jd'])) {
+			// jD's API often returns NULL
+			if (!is_null($events['jd']) && $events['jd'] != 'NULL') {
 				$matches = array_merge($events['jd'], $events['gg']);
 
 				usort($matches, function($key1, $key2) {
 					$value1 = $key1['match_time'];
 					$value2 = $key2['match_time'];
-					return $value1 - $value2;
+					return $value2 - $value1;
 				});
-
 
 				$this->parse($matches);
 			}
@@ -53,7 +52,48 @@ Class Bot {
 	}
 
 	public function parse($matches) {
-		print(json_encode($matches));
+		print'<pre>';
+		print_r($matches);
+		print'</pre>';
+		exit;
+
+		$i = 0;
+		date_default_timezone_set('UTC');
+		foreach ($matches as $match) {
+			//if ($i < $this->limit) {
+				// Time
+				$ticker[$i]['timestamp'] = $match['match_time'];
+				$total_time = time() - $match['match_time'];
+				$days       = floor($total_time /86400);
+				$hours      = floor($total_time /3600);
+				$minutes    = intval(($total_time/60) % 60);
+				$time = "";
+				if($days > 0) $time .= $days . 'd ';
+				else if($hours > 0) $time .= $hours.'h ';
+				else if($minutes > 0) $time .= $minutes.'m';
+				else $time .= 'live';
+
+				$ticker[$i]['time'] = $time;
+
+				// tournament name
+				if (isset($match['coverage_title'])) $ticker[$i]['tournament'] = $match['coverage_title'];
+				else if (isset($match['tournament']['name'])) $ticker[$i]['tournament'] = $match['tournament']['name'];
+
+				// Teams
+				if (isset($match['team_1_name']) && isset($match['team_2_name']))  $ticker[$i]['teams'] = $match['team_1_name'].' vs '.$match['team_2_name'];
+				else if (isset($match['firstOpponent']['name']) && isset($match['secondOpponent']['name']))  $ticker[$i]['teams'] = $match['firstOpponent']['name'].' vs '.$match['secondOpponent']['name'];
+
+				// URLs
+				if (isset($match['pageUrl'])) $ticker[$i]['url']['gg'] = $match['pageUrl'];
+				if (isset($match['coverage_url'])) $ticker[$i]['url']['jd'] = $match['coverage_url'];
+
+				$i++;
+			//} else break;
+		}
+
+		print'<pre>';
+		print_r($ticker);
+		print'</pre>';
 	}
 
 	protected function joinDOTA() {
